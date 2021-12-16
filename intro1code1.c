98b483cf6dfe8778558cf1ddef1e4f5d5fd98aa2
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-16 16:25:59 +0530

    Update intro1code1.c

--- intro1code1.c
+++ intro1code1.c
@@ -1,1 +1,19 @@
+//Take an char string from the user and print it.
 
+#include <stdio.h>
+void main ()
+{
+char str_p [100];
+printf("Enther the welcome word");
+scanf ("%s",str_p);
+printf ("\nyour input = %s",str_p);
+  return 0;
+}
+
+// hellow world.
+
+#include <stdio.h>
+void main()
+{
+    printf("Hello world");
+}
