fd3d80ef93acc1a85eefa95cff91c662748bf3a6
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-29 22:11:11 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,3 +6,15 @@ int main()
 
     return 0;
 }
+
+
+#include <stdio.h>
+int main(void)
+{
+	printf("GeeksQuiz");
+	printf("Krishna");
+	printf("Yashi");
+	printf("Usha");
+	printf("Varun");
+	return 0;
+}
