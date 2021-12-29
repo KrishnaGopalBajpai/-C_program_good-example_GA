eea57f5a901df4a086e497aefad394b74a7a5a87
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-29 23:11:17 +0530

    Update c_input_output.c

--- c_input_output.c
+++ c_input_output.c
@@ -1,2 +1,23 @@
 /*scanf() function to take input from the user, and printf() function to display output to the user.*/
+
 # include <stdio.h>
+int main ()
+{
+  // Display the program in side the quatation.
+  print ("C Programming");
+  return 0;
+}
+
+//
+
+# include <stdio.h>
+int main()
+{
+    int i=5;
+    printf  ("number = %d\n",i);
+  // \n give the new line aftre the 1st printf.
+    printf ("numbers= %d", i);
+    return 0;
+}
+
+
